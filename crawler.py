40422db0b7850811860e1e3bfcc5711e977722f2
sasho kashev <74961253+sanndrino1@users.noreply.github.com>
2022-12-06 15:15:25 +0200

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,8 +1,7 @@
 import requests
 import os
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import re
-BASE_URL = "https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1"
 
 
 
@@ -14,26 +13,26 @@ except:
 	  from libs.constant import DATA_PATH
 
 class Crawler():
-      def __init__(self, base_url):
+    def __init__(self, base_url):
         self.seed=[base_url]
         pass
         
 
   
 
-      def write_to_file(self,filename, content):
+    def write_to_file(self,filename, content):
         """ Write string to given filename
           :param filename: string
          :param content: sring
         """
   
 
-        with open(DATA_PATH+filename,'w', encoding='utf-8') as f:
-          f.write(content)
+        #with open(DATA_PATH+filename,'w', encoding='utf-8') as f:
+          #f.write(content)
 
     
         
-      def get_html(self,url):
+    def get_html(self,url):
         """ Make GET request and save content to file
         First try with SSL verification (default),
         if error => disable SSL verification
@@ -44,37 +43,62 @@ class Crawler():
         if r.ok:
       
           return r.text;
-
-      def scrape_links(self, html):
+		
+		
+    def scrape_links(self,html):
         soup = BeautifulSoup(html,'html.parser')
-        date = list()
-       
+        
+         
+        product=soup.find (id="products-container")
+        divs=product.find_all('div',class_="s1" )
 
+        print(len(divs))
+        for div in divs:
+            price=div.find('div',class_="price")
+			
 
-        prices_div=soup.find(class_='price')
+            a=div.find('a')
+            print(a['href'])
+          
+          #print(price)
+          
 
-        price_divs = prices_div.find_all('div', class_='price')
-        for div in price_divs:
-          price_num = div.span.string
-          print(price_num)
-      def run(self):
-        for url in self.seed:
-          html= self.get_html(url)
-          #self.write_to_file('www.jarcomputers.html.html',html)
-          links = self.scrape_links(html)
-          main_url = 'https://jarcomputers.com/'
-          urls = [ main_url + el  for el in links]
-          print(urls)
-
+            #if price>3000:
+             # a = div.a
+             # print(a.attrs)
+             # date.append(a.attrs)
+              #print(a.attrs)
+                
+		            #print(a.attrs
+		        #data.append(a.attrs)
+		        #print(a.attrs)
 
           
-      
-		
-      
+         
+
+          #print(price)
+         
+				
+
+        
+       
+	       
+        
+
+    def run(self):
+        """ run the crawler for each url in seed
+            Use multithreading for each GET request
+
+        """
+        for url in self.seed:
+          self.get_html(url)
+          html=self.get_html(url)
+          self.write_to_file('jarcomputers.html.html',html)
+          links=self.scrape_links(html)
+
+          print('finishito')
 
 if __name__ == '__main__':
   
-  
-  crawler = Crawler("https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1")
+  crawler = Crawler( "https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1")
   crawler.run()
-
