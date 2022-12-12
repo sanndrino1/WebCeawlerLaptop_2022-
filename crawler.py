a21babe1c159c9ede9995e95510b19c0b0e9b4e1
sasho kashev <74961253+sanndrino1@users.noreply.github.com>
2022-12-12 23:15:02 +0200

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -3,6 +3,8 @@ import os
 from bs4 import BeautifulSoup 
 import re
 
+BASE_URL = "https://www.jarcomputers.com/"
+
 
 
 try:
@@ -14,8 +16,11 @@ except:
 
 class Crawler():
 		def __init__(self, base_url):
-				self.seed=[base_url]
-				pass
+				self.curent_page=1;
+				self.url="https://www.jarcomputers.com/Laptopi_cat_2.html?ref="
+				self.seed=[]
+				
+				
 				
 
 	
@@ -27,8 +32,8 @@ class Crawler():
 				"""
 	
 
-				#with open(DATA_PATH+filename,'w', encoding='utf-8') as f:
-					#f.write(content)
+				with open(DATA_PATH+filename,'w', encoding='utf-8') as f:
+					f.write(content)
 
 		
 				
@@ -43,57 +48,88 @@ class Crawler():
 				if r.ok:
 			
 					return r.text
+		#def bucks_page(self,html):
+			#soup = BeautifulSoup(html,'html.parser')
+			#data=list()
+			#links_lap_top=soup.find(id="products_container")
+			
+			
+			
+			#link_laps=links_lap_top.find_all(class_="p1")
+			#for div in link_laps:
+				#a=div.a
+				#print(a)
+
+				
+				#print(a)
+
+				
 		
 		
-		def price_links(self,html):
+		def mode_price_links(self,html):
 			soup = BeautifulSoup(html,'html.parser')
 			
 			
 			products = soup.find(id="products-container" )
 			print(products.string)
-			divs = products.find_all('div', class_="row-price")
-			print(len(divs))
-				
-			for div in divs:
-				price=div.find('div',class_="price")
-				print(price.text)
-
-		def links_model(self,html):
-			soup = BeautifulSoup(html,'html.parser')
-			brand=soup.find(id="products-container" )
-			print(brand.string)
-			divs=brand.find_all('div',class_="s2")
+			divs = products.find_all('div', class_="s2")
 			#print(len(divs))
+				
 			for div in divs:
-				model=div.find('div',class_="brand-name")
-				
-				print(model.text)
-				rx=re.compile( r'(^$ +[A-Z]+$).')
-				
-
-				m =rx.search(str(model))
-				if m: 
-					model=str(m.group(1))
-					print(model)
-					rx=re.compile('^([A-Z])-?$')
-					m =rx.search(str(model))
-					print(model)
-				
-				
-				
-				
-				
-				
-				
-				
-			
-			
-			
-			
-
+				price=div.find('div',class_="brand-name")
+				print(price.text)
+		
 			
 
 
+		def get_seed(self):
+			page_links=[]
+			
+			page_url=self.url=self.curent_page
+			html=self.get_html(page_url)
+			soup = BeautifulSoup(html,'html.parser')
+		
+			brand=soup.find(id="products-container" )
+			#print(brand.string)
+			divs=brand.find_all('div', class_="row-price") 
+			print(len(divs))
+			for div in divs:
+				model=div.find('div',  lass_="price"  )
+				
+				
+				a=div.find('a')
+				
+				page_links.append(urljoin(BASE_URL, a['href']))
+			if page_links:
+
+				self.seed=[*self.seed,*page_links]
+				self.curent_page+=1
+				self.get_seed()
+
+
+			
+					
+				
+		def ekran_page(self,html):
+			soup = BeautifulSoup(html,'html.parser')
+			date=list()
+			stil_products=soup.find( id="products-container" )
+			print(stil_products.string)
+			
+			stil_product=stil_products.find_all('div',class_="s2")
+			for span in stil_product:
+				ekran=span.find('span', class_="short_title fn" )
+				rx = re.compile(r'[0-9][0-9][0-9]')
+
+				match = re.search(str(ekran))
+				print( match)
+				
+				#print(ekran.text,'uuuuuuuuuuuuuuukkkkkkkkkkkk')
+				
+			
+				
+					
+			
 
 
 
@@ -108,19 +144,15 @@ class Crawler():
 						Use multithreading for each GET request
 
 			"""
-			for url in self.seed:
-					self.get_html(url)
-					html=self.get_html(url)
-					self.write_to_file('jarcomputers.com',html)
-					links=self.price_links(html)
-					#main_url="https://www.jarcomputers.com/"
-					#urls = [ main_url+el  for el in links]
-					#print(urls)
-					page=self.links_model(html)
-					
-					print('finishito')
 
-#if __name__ == '__main__':
+			
+			self.get_seed()
+			
+					
+					
+			print('finishito')
+
+if __name__ == '__main__':
 	
-	#crawler = Crawler( "https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1")
-	#crawler.run()
+	crawler = Crawler( "https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1")
+	crawler.run()
